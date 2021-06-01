787928adf1322f861af3f960b05c059ad204d4ae
PrimaryAaron <>
2021-06-01 02:06:50 +0000

    added test to check that the contents of the array fizzbuzz returns is correct with what I expect from fizzbuzz

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -6,3 +6,8 @@ class test_(unittest.TestCase):
     fizzy = fizzbuzz.fizzBuzz()
     message = "this ain't it, chief. gotta check that type"
     self.assertIsInstance(fizzy, list, message)
+  def test_contents(self):
+    fizzy = fizzbuzz.fizzBuzz()
+    buzzy = [1, 2, "fizz", 4, "buzz", "fizz", 7, 8, "fizz", "buzz", 11, "fizz", 13, 14, "fizzbuzz"]
+    #message = "this ain't it, chief"
+    self.assertEqual(fizzy, buzzy)
