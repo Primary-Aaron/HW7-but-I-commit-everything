35b9b14125d26b3cd84db1bf8c8c1316f3144844
PrimaryAaron <>
2021-06-01 02:07:27 +0000

    implemented fizzbuzz returning an array with actual contents in it

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,4 +2,18 @@ def fizzBuzz():
 
   tempList = []
 
+  for x in range(1, 16):
+    word = ""
+    if (x%3 == 0 or x%5 == 0):
+      if (x%3 == 0):
+        word = "fizz"
+      if (x%5 == 0):
+        word = "buzz"
+      if (x%3 == 0 and x%5 == 0):
+        word = "fizzbuzz"
+    else:
+      word = x
+
+    tempList.append(word)
+
   return tempList
