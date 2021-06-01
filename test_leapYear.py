df00f77effc05648b11697f0b7020a8e385c56d7
PrimaryAaron <>
2021-06-01 02:09:46 +0000

    added test for the 100 years exception

--- test_leapYear.py
+++ test_leapYear.py
@@ -6,3 +6,7 @@ class test_(unittest.TestCase):
     year = leapYear.isLeapYear(8)
     message = "this ain't it, chief"
     self.assertEqual(year, True, message)
+  def test_100(self):
+    year = leapYear.isLeapYear(200)
+    #message = "this ain't it, chief"
+    self.assertEqual(year, False)
